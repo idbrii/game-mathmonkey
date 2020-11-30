ad47c108780a9210acc8e77d14468bc928eaf29f
David Briscoe <idbrii@gmail.com>
2020-11-30 10:18:32 -0800

    Clean up code; add meta files

--- Assets/Plugins/idtool/Editor/EnumArray_Drawer.cs
+++ Assets/Plugins/idtool/Editor/EnumArray_Drawer.cs
@@ -1,23 +1,29 @@
-using UnityEngine;
-using UnityEditor;
-using System.Collections;
 using System.Collections.Generic;
+using System.Collections;
+using UnityEditor;
+using UnityEngine;
+using idbrii.lib.inspector;
 
 namespace idbrii.lib.editor
 {
-    // http://answers.unity.com/answers/1589393/view.html
-    [CustomPropertyDrawer(typeof(EnumNamedArrayAttribute))]
-    public class DrawerEnumNamedArray : PropertyDrawer
+    // Usage:
+    // [EnumArray(typeof(StatsEnum))]
+    // public int[] m_Stats = new int[EnumTool.GetCount<StatsEnum>()];
+    //
+    // Copied from http://answers.unity.com/answers/1589393/view.html
+    [CustomPropertyDrawer(typeof(EnumArrayAttribute))]
+    public class EnumArray_Drawer : PropertyDrawer
     {
-        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
-            EnumNamedArrayAttribute enumNames = attribute as EnumNamedArrayAttribute;
-            //propertyPath returns something like component_hp_max.Array.data[4]
-            //so get the index from there
-            int index = System.Convert.ToInt32(property.propertyPath.Substring(property.propertyPath.IndexOf("[")).Replace("[", "").Replace("]", ""));
-            //change the label
-            label.text = enumNames.names[index];
-            //draw field
-            EditorGUI.PropertyField( position, property, label, true );
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            var enum_attr = attribute as EnumArrayAttribute;
+            // propertyPath returns something like component_hp_max.Array.data[4]
+            // so get the index from there
+            var start = property.propertyPath.IndexOf("[");
+            var index_str = property.propertyPath.Substring(start).Replace("[", "").Replace("]", "");
+            int index = System.Convert.ToInt32(index_str);
+            label.text = enum_attr.Labels[index];
+            EditorGUI.PropertyField(position, property, label, true);
         }
     }
 }
--- Assets/Plugins/idtool/Inspector/EnumArrayAttribute.cs
+++ Assets/Plugins/idtool/Inspector/EnumArrayAttribute.cs
@@ -1,14 +1,14 @@
 using UnityEngine;
 
-namespace idbrii.lib.editor
+namespace idbrii.lib.inspector
 {
     // http://answers.unity.com/answers/1589393/view.html
-    public class EnumNamedArrayAttribute : PropertyAttribute
+    public class EnumArrayAttribute : PropertyAttribute
     {
-        public string[] names;
-        public EnumNamedArrayAttribute(System.Type names_enum_type)
+        public string[] Labels;
+        public EnumArrayAttribute(System.Type names_enum_type)
         {
-            this.names = System.Enum.GetNames(names_enum_type);
+            Labels = System.Enum.GetNames(names_enum_type);
         }
     }
 }
