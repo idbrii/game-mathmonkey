e9e33461f32834ecfb9759948ce621f9ee090a8c
David Briscoe <idbrii@gmail.com>
2020-12-01 00:21:50 -0800

    Improve group shortcut to handle RectTransform

--- Assets/Plugins/idtool/Editor/Shortcuts.cs
+++ Assets/Plugins/idtool/Editor/Shortcuts.cs
@@ -13,12 +13,25 @@ namespace idbrii.lib.editor
             if (!Selection.activeTransform)
                 return;
             var go = new GameObject(Selection.activeTransform.name + " Group");
+            if (Selection.activeTransform is RectTransform)
+            {
+                var rect = go.AddComponent<RectTransform>();
+                Maximize(rect);
+            }
             Undo.RegisterCreatedObjectUndo(go, "Group Selected");
-            // TODO: Set recttransform to fill parent if applicable
+
             go.transform.SetParent(Selection.activeTransform.parent, false);
             foreach (var transform in Selection.transforms)
                 Undo.SetTransformParent(transform, go.transform, "Group Selected");
             Selection.activeGameObject = go;
+        }
+
+        static void Maximize(RectTransform t)
+        {
+            t.anchorMin = Vector2.zero;
+            t.anchorMax = Vector2.one;
+            t.pivot     = Vector2.one * 0.5f;
+            t.sizeDelta = Vector2.zero;
         }
 
     }
